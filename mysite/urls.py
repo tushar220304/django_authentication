1bfd2153270eb0c449dbf22d2e6c6b531b2bdb0b
tushar220304 <63372163+tushar220304@users.noreply.github.com>
2022-07-18 17:59:30 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import handler404
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
+from django.conf.urls import url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
